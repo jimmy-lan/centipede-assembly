4ab542a3723e08c3adb8d15bfc4b382f7fe281e2
Jimmy <jimmylanbcn@gmail.com>
2021-03-24 15:44:10 -0400

    Add main game loop

--- centipede.s
+++ centipede.s
@@ -6,8 +6,8 @@
 # Student: Yanxiang (Jimmy) Lan, 1005463240
 #
 # Bitmap Display Configuration:
-# - Unit width in pixels: 8
-# - Unit height in pixels: 8
+# - Unit width in pixels: 4
+# - Unit height in pixels: 4
 # - Display width in pixels: 256
 # - Display height in pixels: 256
 # - Base Address for Display: 0x10008000 ($gp)
@@ -27,3 +27,51 @@
 # - (write here, if any)
 #
 #####################################################################
+
+.data
+    displayAddress:	 .word 0x10008000
+    backgroundColor: .word 0x00000000
+
+    screenHeight: .word 21
+    screenWeight: .word 21
+
+.globl main
+.text
+
+game_loop_main:
+    # Do something
+
+    jal			sleep				# jump to sleep and save position to $ra
+    
+    j			game_loop_main				# jump to game_loop_main
+
+
+############################################################################################
+	
+program_exit:
+	li $v0, 10 # terminate the program gracefully
+	syscall
+
+############################################################################################
+
+##############################################
+# # Utilities
+##############################################
+
+# FUN sleep
+sleep:
+    addi		$sp, $sp, -20			# $sp -= 20
+    sw			$ra, 0($sp)
+
+    li			$v0, 32				# $v0 = 32
+    li			$a0, 50				# $a0 = 50
+    syscall
+
+    lw			$ra, 0($sp)
+    addi		$sp, $sp, 20			# $sp += 20
+
+    move 		$v0, $zero			# $v0 = $zero
+    jr			$ra					# jump to $ra
+
+# END FUN sleep
+
