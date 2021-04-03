c398d89b9bd83f4ba951bff363ee94957eb961e5
Jimmy <jimmylanbcn@gmail.com>
2021-04-03 14:35:20 -0400

    Add draw_darts function

--- centipede.s
+++ centipede.s
@@ -1012,6 +1012,34 @@ draw_mushroom_at_location:
 
 # END FUN draw_mushroom_at_location
 
+# FUN draw_darts
+# ARGS:
+# $a0: address of the darts array
+# $a1: color of darts
+draw_darts:
+    addi		$sp, $sp, -20			# $sp -= 20
+    sw			$s0, 16($sp)
+    sw			$s1, 12($sp)
+    sw			$s2, 8($sp)
+    sw			$s3, 4($sp)
+    sw			$ra, 0($sp)
+
+    # Load parameters
+    move 		$s0, $a0			    # $s0 = address of the darts array
+    move 		$s1, $a1			    # $s1 = color of darts
+
+    lw			$s0, 16($sp)
+    lw			$s1, 12($sp)
+    lw			$s2, 8($sp)
+    lw			$s3, 4($sp)
+    lw			$ra, 0($sp)
+    addi		$sp, $sp, 20			# $sp += 20
+
+    move 		$v0, $zero			    # $v0 = $zero
+    jr			$ra					    # jump to $ra
+
+# END FUN draw_darts
+
 # FUN draw_dart
 # ARGS:
 # $a0: location (display grid)
