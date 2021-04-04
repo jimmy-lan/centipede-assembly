c4883c5541c7a51b4901821528662b0c6bc5a9f7
Jimmy <jimmylanbcn@gmail.com>
2021-04-04 16:45:11 -0400

    Update fill_background_at_location to reduce repeated code

--- centipede.s
+++ centipede.s
@@ -1170,53 +1170,18 @@ draw_blaster:
     jr			$ra					    # jump to $ra
 
 # END FUN draw_blaster
-
 # FUN fill_background
 # ARGS:
 # $a0: location to fill background (object grid)
 fill_background_at_location:
-    addi		$sp, $sp, -20			    # $sp -= 20
-    sw			$s0, 16($sp)
-    sw			$s1, 12($sp)
-    sw			$s2, 8($sp)
-    sw			$s3, 4($sp)
+    addi		$sp, $sp, -4			    # $sp -= 4
     sw			$ra, 0($sp)
 
-    move 		$s0, $a0			        # $s0 = $a0
-
-    # Calc address
-    move 		$a1, $zero			        # $a1 = $zero
-    jal			calc_display_address	    # jump to calc_display_address and save position to $ra
-    move 		$t2, $v0			        # $t2 = $v0
-
-    # Load colors
-    lw			$t1, screenLineWidth	    # $t1 = screenLineWidth
-    lw			$t9, backgroundColor	    # $t9 = backgroundColor
-
-    # Draw background color at requested location
-    # First line
-    sw			$t9, 0($t2)
-    sw			$t9, 4($t2)
-    sw			$t9, 8($t2)
-
-    # Second line
-    add 		$t2, $t2, $t1			# $t2 = $t2 + $t1, goes to the next line at this location
-    sw			$t9, 0($t2)
-    sw			$t9, 4($t2)
-    sw			$t9, 8($t2)
-
-    # Third line
-    add 		$t2, $t2, $t1			# $t2 = $t2 + $t1, goes to the next line at this location
-    sw			$t9, 0($t2)
-    sw			$t9, 4($t2)
-    sw			$t9, 8($t2)
-
-    lw			$s0, 16($sp)
-    lw			$s1, 12($sp)
-    lw			$s2, 8($sp)
-    lw			$s3, 4($sp)
+    lw			$a1, backgroundColor	    # $t9 = backgroundColor
+    jal			fill_color_at_location		# jump to fill_color_at_location and save position to $ra
+    
     lw			$ra, 0($sp)
-    addi		$sp, $sp, 20			    # $sp += 20
+    addi		$sp, $sp, 4			    # $sp += 4
 
     move 		$v0, $zero			        # $v0 = $zero
     jr			$ra					        # jump to $ra
